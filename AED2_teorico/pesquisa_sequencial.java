2666a8e69b10841261589803f5f9fff1ad2c1459
paulohenrique-m <paulohenriquemenezes00@gmail.com>
2022-02-13 22:46:07 -0300

    add theoretical content

---
+++ AED2_teorico/pesquisa_sequencial.java
@@ -0,0 +1,18 @@
+/*
+basic methods for serching in sequencial files
+it work better if the data is in initial values on array
+worst case: f(n)=n
+best case: f(n)=1
+avarege case: f(n)=(n+1)/2
+*/
+public class pesquisa_sequencial {
+    public static void main(String[] args) {
+        boolean resp = false;
+        for (int i = 0; i < args.length; i++) {
+            if (array[i]==x) {
+                resp = true;
+                i=n;
+            }
+        }
+    }
+}
